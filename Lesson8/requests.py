4ff26a115b82cef7b0ada84a22adf574375b59b5
Vitaly <vitalyvj@users.noreply.github.com>
2020-09-04 03:31:23 +0300

    8th lesson homework

--- Lesson8/requests.py
+++ Lesson8/requests.py
@@ -7,32 +7,32 @@ db = client.instagram
 
 i, j = 0, 0
 
-follow_user = 'machinelearning'
-followers = []
-
-for follower in db.instagram.find({'follower_name': follow_user},
-                                  {'_id': 0,
-                                   'follower_name': 1,
-                                   'follow_name': 1,
-                                   'follow_full_name': 1,
-                                   'follow_pic_url': 1}):
-    followers.append(follower)
-    i += 1
-
-print(f'У пользователя {follow_user} {i} подписок\n')
-pprint(followers)
-
-follower_user = 'datascience'
+follower_user = 'machinelearning'
 follows = []
 
-for follow in db.instagram.find({'follow_name': follower_user},
+for follow in db.instagram.find({'follower_name': follower_user},
                                 {'_id': 0,
-                                 'follow_name': 1,
                                  'follower_name': 1,
-                                 'follower_full_name': 1,
-                                 'follower_pic_url': 1}):
+                                 'follow_name': 1,
+                                 'follow_full_name': 1,
+                                 'follow_pic_url': 1}):
     follows.append(follow)
+    i += 1
+
+print(f'У пользователя {follower_user} {i} подписок\n')
+pprint(follows)
+
+follow_user = 'datascience'
+followers = []
+
+for follower in db.instagram.find({'follow_name': follow_user},
+                                  {'_id': 0,
+                                   'follow_name': 1,
+                                   'follower_name': 1,
+                                   'follower_full_name': 1,
+                                   'follower_pic_url': 1}):
+    followers.append(follower)
     j += 1
 
-print(f'\nУ пользователя {follower_user} {j} подписчиков\n')
-pprint(follows)
+print(f'\nУ пользователя {follow_user} {j} подписчиков\n')
+pprint(followers)
